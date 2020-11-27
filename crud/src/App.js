e5b921a5f17160f710954c2045b42dafb04f673c
WEBEXZ <audi.icon@gmail.com>
2020-11-26 19:29:09 -0600

    New project added

---
+++ crud/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import './App.css';
+
+const name = 'person'
+
+class App extends Component {
+  render() {
+    return <h1>Hello {name}!</h1>
+  }
+}
+
+export default App;
